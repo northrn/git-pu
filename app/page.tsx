a0530a7427ce22d60d4152264ee55f6338a27bfc
northrn <theyunghustler@gmail.com>
2023-09-07 21:13:21 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import '/Users/matthewmertz/Documents/subscription-app/styles/main.css'
+import Pricing from '@/components/Pricing';
+import {
+  getSession,
+  getSubscription,
+  getActiveProductsWithPrices
+} from '@/app/supabase-server';
+
+export default async function PricingPage() {
+  const [session, products, subscription] = await Promise.all([
+    getSession(),
+    getActiveProductsWithPrices(),
+    getSubscription()
+  ]);
+
+  return (
+    <Pricing
+      session={session}
+      user={session?.user}
+      products={products}
+      subscription={subscription}
+    />
+  );
+}
+
