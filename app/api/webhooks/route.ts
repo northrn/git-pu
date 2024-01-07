cfb24a218751b6c7f85814d6474440bcdf78763d
northrn <44729398+northrn@users.noreply.github.com>
2024-01-06 19:40:40 -0500

    Update route.ts

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -3,15 +3,18 @@ import { stripe } from '@/utils/stripe';
 import {
   upsertProductRecord,
   upsertPriceRecord,
+  deleteProductRecord,
+  deletePriceRecord,
   manageSubscriptionStatusChange
 } from '@/utils/supabase-admin';
-import { headers } from 'next/headers';
 
 const relevantEvents = new Set([
   'product.created',
   'product.updated',
+  'product.deleted',
   'price.created',
   'price.updated',
+  'price.deleted',
   'checkout.session.completed',
   'customer.subscription.created',
   'customer.subscription.updated',
@@ -20,13 +23,14 @@ const relevantEvents = new Set([
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const sig = headers().get('Stripe-Signature') as string;
+  const sig = req.headers.get('stripe-signature') as string;
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
   try {
-    if (!sig || !webhookSecret) return;
+    if (!sig || !webhookSecret) throw new Error("Missing Stripe Signature or Webhook Secret!");
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+    console.log(`🔔  Webhook received: ${event.type}`);
   } catch (err: any) {
     console.log(`❌ Error message: ${err.message}`);
     return new Response(`Webhook Error: ${err.message}`, { status: 400 });
@@ -42,6 +46,12 @@ export async function POST(req: Request) {
         case 'price.created':
         case 'price.updated':
           await upsertPriceRecord(event.data.object as Stripe.Price);
+          break;
+        case 'product.deleted':
+          await deleteProductRecord(event.data.object as Stripe.Product);
+          break;
+        case 'price.deleted':
+          await deletePriceRecord(event.data.object as Stripe.Price);
           break;
         case 'customer.subscription.created':
         case 'customer.subscription.updated':
